3aa27a1e1197589b3a9d88a52eec02d0dec4affe
JoshuaKey <keyjoshj98@yahoo.com>
2018-10-02 19:56:43 -0600

    Initial Commit

---
+++ EasyMarketingInUnityVS/EasyMarketingInUnityConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMarketingInUnityConsole {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine(EasyMarketingInUnity.Print());
+
+        }
+    }
+}
